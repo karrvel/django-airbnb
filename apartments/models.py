c742a9704dce60ff0cbaab8e911b729d3f1d886a
karrvel <qosimovx20@gmail.com>
2024-11-23 17:53:45 +0500

    apartments model added

--- apartments/models.py
+++ apartments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Apartment(models.Model):
+    class StatusChoices(models.TextChoices):
+        REVIEW = ('r', 'REVIEW')
+        REJECT = ('x', 'REJECT')
+        ACCEPT = ('a', 'ACCEPT')
+
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    address = models.CharField(max_length=200)
+    owner = models.ForeignKey('users.Users', on_delete=models.RESTRICT)
+    desciprtion = models.TextField()
+    image = models.ImageField()
+    status = models.CharField(max_length=1, choices=StatusChoices, default=StatusChoices.REVIEW)
