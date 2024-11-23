d1d755fee3f835dbedb5942a18f4fd689ce60098
karrvel <qosimovx20@gmail.com>
2024-11-23 17:56:45 +0500

    hiufydtjhggkhg;flyjhkhiugy

--- apartments/views.py
+++ apartments/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django import http
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.views import View
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+# @csrf_exempt
+# def hello_world(request: HttpRequest) -> HttpResponse:
+#     if request.method == "GET":
+#         return HttpResponse("<b>hello world</b>")
+#     if request.method == "POST":
+#         return HttpResponse(f"{datetime.now()}")
+
+
+class HelloWorld(View):
+    def get(self, request):
+        return HttpResponse("<b>hello world</b>")
+    
+    @csrf_exempt
+    def post(self, request):
+         return HttpResponse(f"{datetime.now()}")
